3d27175a813eb708d3ec9a92aca7b752e7b62eff
azenku <azenku@NN123-HA-19.HS.sd203.org>
2015-08-21 14:13:51 -0500

    rock an rool

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -3,33 +3,27 @@ import java.awt.Color;
 public class TurtleWorld
 {
     public static void main(String args[])
+    throws InterruptedException
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
         turtle.setPenColor(Color.RED);
         turtle.penDown();
-        turtle.forward(50);
-        turtle.backward(30);
-        turtle.turnLeft();
-        turtle.forward(50);
-        turtle.forward(50);
-        turtle.backward(30);
-        turtle.turnLeft();
-        turtle.forward(50);turtle.forward(50);
-        turtle.backward(30);
-        turtle.turnLeft();
-        turtle.forward(50);turtle.forward(50);
-        turtle.backward(30);
-        turtle.turnLeft();
-        turtle.forward(50);turtle.forward(50);
-        turtle.backward(30);
-        turtle.turnLeft();
-        turtle.forward(50);turtle.forward(50);
-        turtle.backward(30);
-        turtle.turnLeft();
-        turtle.forward(50);turtle.forward(50);
-        turtle.backward(30);
-        turtle.turnLeft();
-        turtle.forward(50);
+        
+        for (int i=1; i<9999; i++){
+            turtle.forward(2);
+            turtle.backward(1);
+            turtle.turn(3);
+            Thread.sleep(2); // sleep 1000 milliseconds (1 second);
+        }
+        
+        for (int i=1; i<9999; i++){
+            turtle.setPenColor(Color.BLUE);
+            turtle.forward(5);
+            Thread.sleep(5);
+            turtle.turn(3);
+            turtle.backward(12);
+            Thread.sleep(5);
+        }
     }
 }
