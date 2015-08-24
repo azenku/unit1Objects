570147cb56a21d8a0eeac4fe2bd8796d8ee40c4d
azenku <azenku@NN123-HA-19.HS.sd203.org>
2015-08-24 14:13:13 -0500

    dankdank

---
+++ chap1 practice/HelloPrinter.java
@@ -0,0 +1,10 @@
+public class HelloPrinter
+{
+    public static void main( String[] args )
+    {
+        //display a greeting
+        
+        
+        System.out.println("hello, World!");
+    }
+}
