5493b526c57586ec4e1eb3b5a222e3e7b82e7e65
azenku <azenku@NN123-HA-19.HS.sd203.org>
2015-08-25 14:14:10 -0500

    awesoem

---
+++ hello2/Dialog_viewer.java
@@ -0,0 +1,16 @@
+
+
+import javax.swing.JOptionPane;
+
+
+public class Dialog_viewer
+{
+   public static void main(String[] ags)
+   {
+       String name = JOptionPane.showInputDialog("What is your name?");
+       System.out.println(name);
+       System.out.println("sucks major poo");
+       
+    }
+
+}
