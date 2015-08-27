72b3d4df1e08c222bb7d685e0e3ab6f8a5ac087f
azenku <azenku@NN123-HA-19.HS.sd203.org>
2015-08-27 14:12:42 -0500

    op

---
+++ chapter 2 practice/String1.java
@@ -0,0 +1,12 @@
+public class String1
+{
+    public static void main( String[] args )
+    {
+        String greeting = "Hello World!";
+        int n = greeting.length();
+        
+        
+        System.out.println( "length = " + n);
+        
+    }
+}
